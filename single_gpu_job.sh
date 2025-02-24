b65eee0b6fb3411b4cd51b9bdabee9f6d3337780
Krish Ranjan <52721908+kr45732@users.noreply.github.com>
2025-02-24 00:05:08 -0500

    Update single_gpu_job.sh

--- single_gpu_job.sh
+++ single_gpu_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=benchmark
-#SBATCH -N1 --ntasks-per-node=1
+#SBATCH -N1 -n8 --ntasks-per-node=1
 #SBATCH --gpus=1
 #SBATCH --time=00:59:99
 
 module load anaconda3
 source activate my_env
 
-pip install transformers pillow torch pandas opencv-python matplotlib psutil
+pip install transformers pillow torch pandas opencv-python matplotlib psutil pynvml
 
 echo "Starting Benchmark..."
 start=$(date +%s)
